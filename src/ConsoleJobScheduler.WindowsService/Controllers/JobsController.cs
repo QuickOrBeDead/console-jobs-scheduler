bd65f1e12a17f3bd0004aa5aaf3246f3823818d1
QuickOrBeDead <boraakgn@gmail.com>
2023-06-22 20:19:23 +0300

    add jobs controller

---
+++ src/ConsoleJobScheduler.WindowsService/Controllers/JobsController.cs
@@ -0,0 +1,47 @@
+﻿namespace ConsoleJobScheduler.WindowsService.Controllers;
+
+using ConsoleJobScheduler.WindowsService.Scheduler.Models;
+using ConsoleJobScheduler.WindowsService.Scheduler;
+using Microsoft.AspNetCore.Mvc;
+using Quartz;
+
+[Route("api/[controller]")]
+[ApiController]
+public sealed class JobsController : Controller
+{
+    private readonly ISchedulerService _schedulerService;
+
+    public JobsController(ISchedulerService schedulerService)
+    {
+        _schedulerService = schedulerService ?? throw new ArgumentNullException(nameof(schedulerService));
+    }
+
+    [HttpGet]
+    public async Task<IList<JobListItemModel>> Get()
+    {
+        return await _schedulerService.GetJobList();
+    }
+
+    [HttpGet("{group}/{name}", Name = "Get")]
+    public async Task<IActionResult> Get(string group, string name)
+    {
+        var jobKey = new JobKey(name, group);
+        var jobDetail = await _schedulerService.GetJobDetail(jobKey);
+        if (jobDetail == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(
+            new JobDetailModel
+                {
+                    JobName = jobDetail.JobName,
+                    JobGroup = jobDetail.JobGroup,
+                    Description = jobDetail.Description,
+                    CronExpression = jobDetail.CronExpression,
+                    CronExpressionDescription = jobDetail.CronExpressionDescription,
+                    Package = jobDetail.Package,
+                    Parameters = jobDetail.Parameters
+                });
+    }
+}
