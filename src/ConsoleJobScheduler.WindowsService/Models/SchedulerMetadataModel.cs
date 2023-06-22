6b5726cc6140d84259873339c52b798c1cf35b03
QuickOrBeDead <boraakgn@gmail.com>
2023-06-22 21:18:41 +0300

    add SchedulerController

---
+++ src/ConsoleJobScheduler.WindowsService/Models/SchedulerMetadataModel.cs
@@ -0,0 +1,28 @@
+﻿namespace ConsoleJobScheduler.WindowsService.Models;
+
+public sealed class SchedulerMetadataModel
+{
+    public string? SchedulerName { get; set; }
+
+    public string? SchedulerInstanceId { get; set; }
+
+    public string? SchedulerType { get; set; }
+
+    public bool SchedulerRemote { get; set; }
+
+    public bool Started { get; set; }
+
+    public bool InStandbyMode { get; set; }
+
+    public bool Shutdown { get; set; }
+
+    public string? JobStoreType { get; set; }
+
+    public string? ThreadPoolType { get; set; }
+
+    public int ThreadPoolSize { get; set; }
+
+    public string? Version { get; set; }
+
+    public string? Summary { get; set; }
+}
