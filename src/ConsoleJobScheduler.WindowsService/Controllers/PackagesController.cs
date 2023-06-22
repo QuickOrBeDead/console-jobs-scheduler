9ba06ebefe1d3816709fea1ad67cb044998a9708
QuickOrBeDead <boraakgn@gmail.com>
2023-06-22 20:23:57 +0300

    add packages controller

---
+++ src/ConsoleJobScheduler.WindowsService/Controllers/PackagesController.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleJobScheduler.WindowsService.Controllers;
+
+using ConsoleJobScheduler.WindowsService.Scheduler;
+using Microsoft.AspNetCore.Mvc;
+
+[Route("api/[controller]")]
+[ApiController]
+public class PackagesController : Controller
+{
+    private readonly ISchedulerService _schedulerService;
+
+    public PackagesController(ISchedulerService schedulerService)
+    {
+        _schedulerService = schedulerService ?? throw new ArgumentNullException(nameof(schedulerService));
+    }
+
+    [HttpGet]
+    public IList<string> Get()
+    {
+        return _schedulerService.GetPackages();
+    }
+}
