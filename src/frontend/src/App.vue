2d048842410edebeef201e1f59589c0ff997cf6c
QuickOrBeDead <boraakgn@gmail.com>
2023-07-07 23:45:44 +0300

    call schedulerApi apiSchedulerGet

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -24,6 +24,7 @@
       </div>
     </div>
   </nav>
+  <router-view></router-view>
 </template>
 
 <style scoped>
--- src/frontend/src/components/Scheduler.vue
+++ src/frontend/src/components/Scheduler.vue
@@ -1,5 +1,13 @@
 <script setup lang="ts">
+import { onMounted } from 'vue';
+import { createApi } from '../api'
+import { SchedulerApi } from '../metadata/console-jobs-scheduler-api'
 
+onMounted(async () => {
+    const schedulerApi = createApi(SchedulerApi)
+    const data = await schedulerApi.apiSchedulerGet()
+    console.log(data.data)
+})
 </script>
 
 <template>
