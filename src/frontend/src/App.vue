42a07a5c94f63e00918e8f88cd842d0cc355c6da
QuickOrBeDead <boraakgn@gmail.com>
2023-07-07 22:44:09 +0300

    add vue router

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -1,18 +1,18 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+
 </script>
 
 <template>
   <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">Console Jobs</a>
+      <a class="navbar-brand" href="/">Console Jobs</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarColor02">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Scheduler</a>
+            <a class="nav-link active" aria-current="page" href="/">Scheduler</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="#">Jobs</a>
@@ -24,7 +24,6 @@ import HelloWorld from './components/HelloWorld.vue'
       </div>
     </div>
   </nav>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
--- src/frontend/src/main.ts
+++ src/frontend/src/main.ts
@@ -1,7 +1,15 @@
-import { createApp } from 'vue'
 import './style.css'
-import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.min.js'
 
-createApp(App).mount('#app')
+import App from './App.vue'
+import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
+
+import Scheduler from './components/Scheduler.vue'
+
+const routes = [
+{ path: '/', component: Scheduler }
+]
+
+createApp(App).use(createRouter({  history : createWebHistory(), routes: routes })).mount('#app')
