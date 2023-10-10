adc89fc6a823b44e3f91f60c0c6c4fe1082e4a8a
QuickOrBeDead <boraakgn@gmail.com>
2023-10-10 19:13:14 +0300

    call UpdateJobHistoryEntryLastSignalTime every 10 seconds

--- src/backend/src/ConsoleJobScheduler.Service/Infrastructure/Scheduler/Jobs/ConsoleAppPackageJob.cs
+++ src/backend/src/ConsoleJobScheduler.Service/Infrastructure/Scheduler/Jobs/ConsoleAppPackageJob.cs
@@ -1,5 +1,7 @@
 ﻿namespace ConsoleJobScheduler.Service.Infrastructure.Scheduler.Jobs;
 
+using ConsoleJobScheduler.Service.Infrastructure.Extensions;
+
 using Quartz;
 
 [PersistJobDataAfterExecution]
@@ -27,6 +29,59 @@ public sealed class ConsoleAppPackageJob : IJob
             throw new InvalidOperationException("jobData.GetString(\"parameters\") is null");
         }
 
-        await _consoleAppPackageRunner.Run(context.FireInstanceId, package, parameters, context.CancellationToken).ConfigureAwait(false);
+        var cancellationTokenSource = new CancellationTokenSource();
+        try
+        {
+            async Task UpdateJobLastSignalTime(string jobRunId, CancellationToken c)
+            {
+                while (!c.IsCancellationRequested)
+                {
+                    try
+                    {
+                        await context.Scheduler.GetJobHistoryDelegate().UpdateJobHistoryEntryLastSignalTime(jobRunId, c);
+                    }
+                    catch
+                    {
+                       // Empty
+                       // TODO: log
+                    }
+
+                    try
+                    {
+                        // ReSharper disable once MethodSupportsCancellation
+#pragma warning disable CA2016 // Forward the 'CancellationToken' parameter to methods
+                        await Task.Delay(10_000, c).ContinueWith(_ => Task.CompletedTask);
+#pragma warning restore CA2016 // Forward the 'CancellationToken' parameter to methods
+                    }
+                    catch (OperationCanceledException)
+                    {
+                        // Empty
+                    }
+                }
+            }
+
+            var signalTask = UpdateJobLastSignalTime(context.FireInstanceId, cancellationTokenSource.Token);
+            await _consoleAppPackageRunner.Run(context.FireInstanceId, package, parameters, context.CancellationToken).ConfigureAwait(false);
+
+            cancellationTokenSource.Cancel();
+
+            try
+            {
+                await signalTask;
+            }
+            catch (OperationCanceledException)
+            {
+                // Empty
+            }
+            catch (Exception)
+            {
+                // Empty
+                // TODO: log
+            }
+        }
+        finally
+        {
+            cancellationTokenSource.Cancel();
+        }
     }
 }
