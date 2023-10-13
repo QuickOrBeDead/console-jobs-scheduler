0217673b9dd2f93eac411e9b39b66b9c615520aa
QuickOrBeDead <boraakgn@gmail.com>
2023-10-13 23:55:23 +0300

    configure routing authorization

--- src/frontend/src/main.ts
+++ src/frontend/src/main.ts
@@ -7,27 +7,65 @@ import App from './App.vue'
 import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
+import { AuthApi } from './metadata/console-jobs-scheduler-api'
+import { createApi } from './api'
+import axios from 'axios'
+
 import Pagination from './components/common/Pagination.vue'
 import Scheduler from './components/Scheduler.vue'
+import Login from './components/Login.vue'
 import JobsList from './components/jobs/List.vue'
 import JobsEdit from './components/jobs/Edit.vue'
 import HistoryList from './components/history/List.vue'
 import JobExecutionDetail from './components/history/Detail.vue'
 import PackagesList from './components/packages/List.vue'
 import PackagesEdit from './components/packages/Edit.vue'
+import { AuthHelper } from './authHelper'
 
 const routes = [
-{ path: '/', component: Scheduler },
-{ path: '/jobs', component: JobsList },
-{ path: '/jobs/edit', component: JobsEdit, name: 'EditJob', props: true },
-{ path: '/history/:jobName?', component: HistoryList, name: 'JobHistory' },
-{ path: '/history/details/:id', component: JobExecutionDetail, name: 'JobExecutionDetails', props: true },
-{ path: '/packages', component: PackagesList },
-{ path: '/packages/details/:name?', component: PackagesEdit, name: 'EditPackage' }
+{ path: '/', component: Scheduler, meta: { requiresAuth: true } },
+{ path: '/login', component: Login, name: 'Login' },
+{ path: '/jobs', component: JobsList, meta: { requiresAuth: true } },
+{ path: '/jobs/edit', component: JobsEdit, name: 'EditJob', props: true, meta: { requiresAuth: true } },
+{ path: '/history/:jobName?', component: HistoryList, name: 'JobHistory', meta: { requiresAuth: true } },
+{ path: '/history/details/:id', component: JobExecutionDetail, name: 'JobExecutionDetails', props: true, meta: { requiresAuth: true } },
+{ path: '/packages', component: PackagesList, meta: { requiresAuth: true } },
+{ path: '/packages/details/:name?', component: PackagesEdit, name: 'EditPackage', meta: { requiresAuth: true } }
 ]
 
+const router = createRouter({ history: createWebHistory(), routes: routes, linkActiveClass: 'active' })
+
+const authHelper = AuthHelper.getInstance()
+const authApi = createApi(AuthApi)
+
+router.beforeEach(async (to, _, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    const user = (await authApi.apiAuthGetUserGet())?.data
+    if (user) {
+      if (authHelper.onAuthenticate) {
+        authHelper.onAuthenticate(user)
+      }
+      
+      next()
+    } else {
+      next({ name: 'Login' })
+    }
+  } else {
+    next()
+  }
+})
+
+axios.interceptors.response.use((r: any) => {
+    return r
+ }, (error: any) => {
+   if (error.response.status === 401 || error.response.status === 403) {
+    router.push({ path: '/' })
+   }
+   return error
+ })
+
 createApp(App)
-    .use(createRouter({  history : createWebHistory(), routes: routes, linkActiveClass: 'active' }))
+    .use(router)
     .component('pagination', Pagination)
     .mount('#app')
 
