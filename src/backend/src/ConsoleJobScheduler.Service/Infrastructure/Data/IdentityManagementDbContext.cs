a17d41f87ea5f158b2f21ec00d6619c325eae172
QuickOrBeDead <boraakgn@gmail.com>
2023-10-13 23:47:31 +0300

    add Identity migrations

---
+++ src/backend/src/ConsoleJobScheduler.Service/Infrastructure/Data/IdentityManagementDbContext.cs
@@ -0,0 +1,25 @@
+﻿namespace ConsoleJobScheduler.Service.Infrastructure.Data;
+
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+public sealed class IdentityManagementDbContext : IdentityDbContext<IdentityUser<int>, IdentityRole<int>, int>
+{
+    public IdentityManagementDbContext(DbContextOptions<IdentityManagementDbContext> options) : base(options)
+    {
+    }
+}
+
+
+public class IdentityManagementDbContextFactory : IDesignTimeDbContextFactory<IdentityManagementDbContext>
+{
+    public IdentityManagementDbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<IdentityManagementDbContext>();
+        optionsBuilder.UseNpgsql("User ID=quartz;Password=quartz;Host=localhost;Port=5432;Database=quartz;Pooling=true;Minimum Pool Size=0;Maximum Pool Size=105;");
+
+        return new IdentityManagementDbContext(optionsBuilder.Options);
+    }
+}
