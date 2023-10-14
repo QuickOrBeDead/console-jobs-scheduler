69a0f072a562eef85612ed9f86aa22dad4f35fef
QuickOrBeDead <boraakgn@gmail.com>
2023-10-15 00:04:24 +0300

    add UsersController

---
+++ src/backend/src/ConsoleJobScheduler.Service/Api/Models/UserAddOrUpdateModel.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleJobScheduler.Service.Api.Models;
+
+using System.ComponentModel.DataAnnotations;
+
+public sealed class UserAddOrUpdateModel
+{
+    private IList<string>? _roles;
+
+    public int Id { get; set; }
+
+    [Required, MaxLength(20)]
+    public string? UserName { get; set; }
+
+    [MaxLength(8)]
+    public string? Password { get; set; }
+
+    public IList<string> Roles
+    {
+        get => _roles ??= new List<string>();
+        set => _roles = value;
+    }
+}
