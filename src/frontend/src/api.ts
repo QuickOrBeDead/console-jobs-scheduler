5f2b52e8ccebe9c5b511c163af5d50fe968a8fdb
QuickOrBeDead <boraakgn@gmail.com>
2023-10-19 12:39:09 +0300

    add callApi function that handles validation errors

--- src/frontend/src/api.ts
+++ src/frontend/src/api.ts
@@ -1,7 +1,26 @@
+import { AxiosError, AxiosResponse } from "axios"
 import { BaseAPI } from "./metadata/console-jobs-scheduler-api/base"
+import { ValidationProblemDetails } from "./metadata/console-jobs-scheduler-api"
+import { Ref } from "vue"
 
 export function createApi<T extends BaseAPI>(ctor: { new (): T }) : T {
     const api = new ctor()
     api["basePath"] = "http://localhost:5000"
     return api
+}
+
+export async function callApi<T>(func: () => Promise<AxiosResponse<T, any>>, validationErrorsRef: Ref<{[key: string]: string[]}>): Promise<{data: T | undefined, hasError: boolean}> {
+    try {
+        validationErrorsRef.value = {}
+        return { data: (await func()).data, hasError: false }
+    } catch (error: unknown) {
+        const axiosError = error as AxiosError<ValidationProblemDetails, any>
+        if (axiosError && axiosError.response && axiosError.response.data && axiosError.response.data.errors)
+        {
+            validationErrorsRef.value = axiosError.response.data.errors
+            return { data: undefined, hasError: true }
+        }
+
+        throw error
+    }
 }
