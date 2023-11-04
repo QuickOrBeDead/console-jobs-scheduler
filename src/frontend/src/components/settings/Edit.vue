ba9fb205e2a870344e5b32c8494f6d41a8ae202e
QuickOrBeDead <boraakgn@gmail.com>
2023-11-05 01:21:07 +0300

    add edit settings component

---
+++ src/frontend/src/components/settings/Edit.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import Smtp from './Smtp.vue'
+import General from './General.vue'
+import { ref } from 'vue'
+
+const currentTab = ref('General')
+
+const tabs: { [key: string]: any } = {
+  'General': General,
+  'Smtp': Smtp
+}
+</script>
+
+<template>
+    <div class="page-container">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-8">
+                    <ul class="nav nav-tabs mb-3" role="tablist">
+                        <li class="nav-item" role="presentation" v-for="(_, tab) in tabs">
+                            <button @click="() => currentTab = tab as string" :class="['nav-link', { active: currentTab === tab }]" :id="`${tab}-tab-button`" data-bs-toggle="pill" type="button" role="tab" aria-controls="settings-tab" aria-selected="true">
+                                {{ tab }}
+                            </button>
+                        </li>
+                    </ul>
+                </div>
+                <div class="tab-content">
+                    <div class="tab-pane fade show active" id="settings-tab" role="tabpanel" :aria-labelledby="`${currentTab}-tab-button`">
+                        <component :is="tabs[currentTab] as string"></component>  
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
