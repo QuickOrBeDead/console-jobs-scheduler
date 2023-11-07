1a569d6527e848aec1ac080a472cb5cff7680f19
QuickOrBeDead <boraakgn@gmail.com>
2023-11-07 08:59:25 +0300

    update scheduler component ui design

--- src/frontend/src/components/Scheduler.vue
+++ src/frontend/src/components/Scheduler.vue
@@ -19,200 +19,174 @@ onMounted(async () => {
 <template>
 <div class="page-container">
     <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <h4 class="display-6" style="margin-bottom: 0;">Statistics</h4>
-                <hr style="margin: 4px 0px;">
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-md-2">
-                <div class="counter-box" style="background-color: #0c5a0c; background-image: linear-gradient(#107c10, #0c5a0c);">
-                    <i class="bi bi-check"></i>
-                    <span class="counter">{{ statistics ? statistics.totalSucceededJobs : 0 }}</span>
-                    <p>Jobs Succeeded</p>
+        <div class="card mb-4">
+            <div class="card-header"><h2 class="display-6 mb-0 mt-0">Statistics</h2></div>
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-sm-6">
+                        <div class="row">
+                            <div class="col-6">
+                                <div class="border-start border-5 border-success px-3 mb-3"><span><i class="bi bi-check"></i> Jobs Succeeded</span>
+                                <div class="fs-1 fw-semibold">{{ statistics ? statistics.totalSucceededJobs : 0 }}</div>
+                                </div>
+                            </div>
+                            <div class="col-6">
+                                <div class="border-start border-5 border-danger px-3 mb-3"><span><i class="bi bi-x"></i> Jobs Failed</span>
+                                <div class="fs-1 fw-semibold">{{ statistics ? statistics.totalFailedJobs : 0 }}</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="row">
+                            <div class="col-6">
+                                <div class="border-start border-5 border-info px-3 mb-3"><span><i class="bi bi-play"></i> Jobs Running</span>
+                                <div class="fs-1 fw-semibold">{{ statistics ? statistics.totalRunningJobs : 0 }}</div>
+                                </div>
+                            </div>
+                            <div class="col-6">
+                                <div class="border-start border-5 border-warning px-3 mb-3"><span><i class="bi bi-stop"></i> Jobs Vetoed</span>
+                                <div class="fs-1 fw-semibold">{{ statistics ? statistics.totalVetoedJobs : 0 }}</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div class="col-md-2">
-                <div class="counter-box" style="background-color: #c42121; background-image: linear-gradient(#db2828, #c42121);">
-                    <i class="bi bi-x"></i>
-                    <span class="counter">{{ statistics ? statistics.totalFailedJobs : 0 }}</span>
-                    <p>Jobs Failed</p>
+        </div>
+        <div class="row">
+            <div class="col-sm-6">
+                <div class="card mb-4">
+                    <div class="card-header"><h3 class="mb-0 mt-0">Scheduler</h3></div>
+                    <div class="card-body p-0">
+                        <table class="table table-condensed table-hover mb-0">
+                            <tbody>
+                            <tr>
+                                <th scope="row">Name</th>
+                                <td>{{ metadata?.schedulerName }}</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">Instance Id</th>
+                                <td>{{ metadata?.schedulerInstanceId }}</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">Type</th>
+                                <td>{{ metadata?.schedulerType }}</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">Remote</th>
+                                <td>{{ metadata?.schedulerRemote ? "TRUE" : "FALSE" }}</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">Version</th>
+                                <td>{{ metadata?.version }}</td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
-            <div class="col-md-2">
-                <div class="counter-box" style="background-color: #1c70b0; background-image: linear-gradient(#2185d0, #1c70b0);">
-                    <i class="bi bi-play"></i>
-                    <span class="counter">{{ statistics ? statistics.totalRunningJobs : 0 }}</span>
-                    <p>Jobs Running</p>
+            <div class="col-sm-6">
+                <div class="card mb-4">
+                    <div class="card-header"><h3 class="mb-0 mt-0">Status</h3></div>
+                    <div class="card-body p-0">
+                        <table class="table table-condensed table-hover mb-0">
+                            <tbody>
+                            <tr>
+                                <th scope="row">Running Since</th>
+                                <td>{{ metadata?.runningSince?.toLocaleDateTimeString() }}</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">Started</th>
+                                <td>{{ metadata?.started ? "TRUE" : "FALSE" }}</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">In Standby Mode</th>
+                                <td>{{ metadata?.inStandbyMode ? "TRUE" : "FALSE" }}</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">Shutdown</th>
+                                <td>{{ metadata?.shutdown ? "TRUE" : "FALSE" }}</td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
-            <div class="col-md-2">
-                <div class="counter-box" style="background-color: rgb(235, 178, 8); background-image: linear-gradient(rgb(198, 153, 16), rgb(235, 178, 8));">
-                    <i class="bi bi-stop"></i>
-                    <span class="counter">{{ statistics ? statistics.totalVetoedJobs : 0 }}</span>
-                    <p>Jobs Vetoed</p>
+        </div>
+        <div class="row">
+            <div class="col-sm-6">
+                <div class="card mb-4">
+                    <div class="card-header"><h3 class="mb-0 mt-0">Job Store</h3></div>
+                    <div class="card-body p-0">
+                        <table class="table table-condensed table-hover mb-0">
+                            <tbody>
+                            <tr>
+                                <th scope="row">Type</th>
+                                <td>{{ metadata?.jobStoreType }}</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">Clustered</th>
+                                <td>{{ metadata?.jobStoreClustered ? "TRUE" : "FALSE" }}</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">Supports Persistence</th>
+                                <td>{{ metadata?.jobStoreSupportsPersistence ? "TRUE" : "FALSE" }}</td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
-        </div>	
-        <div class="row">
-            <div class="col-md-6">
-                <h4 class="display-6" style="margin-bottom: 0;">Status</h4>
-                <hr style="margin: 4px 0px;">
-            </div>
-            <div class="col-md-6">
-                <h4 class="display-6" style="margin-bottom: 0;">Scheduler</h4>
-                <hr style="margin: 4px 0px;">
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-6">
-                <table class="table table-striped table-bordered">
-                    <tbody>
-                    <tr>
-                        <th scope="row">Running Since</th>
-                        <td>{{ metadata?.runningSince?.toLocaleDateTimeString() }}</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">Started</th>
-                        <td>{{ metadata?.started ? "TRUE" : "FALSE" }}</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">In Standby Mode</th>
-                        <td>{{ metadata?.inStandbyMode ? "TRUE" : "FALSE" }}</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">Shutdown</th>
-                        <td>{{ metadata?.shutdown ? "TRUE" : "FALSE" }}</td>
-                    </tr>
-                    </tbody>
-                </table>
-            </div>
-            <div class="col-6">
-                <table class="table table-striped table-bordered">
-                    <tbody>
-                    <tr>
-                        <th scope="row">Name</th>
-                        <td>{{ metadata?.schedulerName }}</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">Instance Id</th>
-                        <td>{{ metadata?.schedulerInstanceId }}</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">Type</th>
-                        <td>{{ metadata?.schedulerType }}</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">Remote</th>
-                        <td>{{ metadata?.schedulerRemote ? "TRUE" : "FALSE" }}</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">Version</th>
-                        <td>{{ metadata?.version }}</td>
-                    </tr>
-                    </tbody>
-                </table>
+            <div class="col-sm-6">
+                <div class="card mb-4">
+                    <div class="card-header"><h3 class="mb-0 mt-0">Thread Pool</h3></div>
+                    <div class="card-body p-0">
+                        <table class="table table-condensed table-hover mb-0">
+                            <tbody>
+                            <tr>
+                                <th scope="row">Type</th>
+                                <td>{{ metadata?.threadPoolType }}</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">Size</th>
+                                <td>{{ metadata?.threadPoolSize }}</td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
             </div>
         </div>
         <div class="row">
-            <div class="col-md-6">
-                <h4 class="display-6" style="margin-bottom: 0;">Job Store</h4>
-                <hr style="margin: 4px 0px;">
-            </div>
-            <div class="col-md-6">
-                <h4 class="display-6" style="margin-bottom: 0;">Thread Pool</h4>
-                <hr style="margin: 4px 0px;">
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-6">
-                <table class="table table-striped table-bordered">
-                    <tbody>
-                    <tr>
-                        <th scope="row">Type</th>
-                        <td>{{ metadata?.jobStoreType }}</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">Clustered</th>
-                        <td>{{ metadata?.jobStoreClustered ? "TRUE" : "FALSE" }}</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">Supports Persistence</th>
-                        <td>{{ metadata?.jobStoreSupportsPersistence ? "TRUE" : "FALSE" }}</td>
-                    </tr>
-                    </tbody>
-                </table>
-            </div>
-            <div class="col-6">
-                <table class="table table-striped table-bordered">
-                    <tbody>
-                    <tr>
-                        <th scope="row">Type</th>
-                        <td>{{ metadata?.threadPoolType }}</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">Size</th>
-                        <td>{{ metadata?.threadPoolSize }}</td>
-                    </tr>
-                    </tbody>
-                </table>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-md-12">
-                <h4 class="display-6" style="margin-bottom: 0;">Nodes</h4>
-                <hr style="margin: 4px 0px;">
-            </div>
-        </div>
-        <div class="row" v-for="node in nodes">
-            <div class="col-12">
-                <table class="table table-striped table-bordered">
-                    <tbody>
-                    <tr>
-                        <th scope="row">Instance Id</th>
-                        <td>{{ node.schedulerInstanceId }}</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">Check-in Interval</th>
-                        <td>{{ node.checkInInterval }}</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">Check-in Timestamp</th>
-                        <td>{{ node.checkInTimestamp?.toLocaleDateTimeString() }}</td>
-                    </tr>
-                    </tbody>
-                </table>
+            <div class="col-sm-12">
+                <div class="card mb-4">
+                    <div class="card-header"><h3 class="mb-0 mt-0">Nodes</h3></div>
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-sm-6" v-for="node in nodes">
+                                <table class="table table-condensed table-hover">
+                                    <tbody>
+                                    <tr>
+                                        <th scope="row">Instance Id</th>
+                                        <td>{{ node.schedulerInstanceId }}</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">Check-in Interval</th>
+                                        <td>{{ node.checkInInterval }}</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">Check-in Timestamp</th>
+                                        <td>{{ node.checkInTimestamp?.toLocaleDateTimeString() }}</td>
+                                    </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 </div>
 </template>
-
-<style scoped>
-.counter-box {
-    border-radius: 4px;
-    text-align: center;
-}
-
-.counter-box p {
-	color: #fff;
-	font-size: 18px;
-    text-shadow: 1px 2px 4px rgba(0,0,0,0.25);
-}
-
-.counter-box i {
-	font-size: 60px;
-	color: #fff;
-    line-height: 70px;
-    text-shadow: 1px 2px 4px rgba(0,0,0,0.25);
-}
-
-.counter { 
-	font-size: 64px;
-	color: #fff;
-	line-height: 28px;
-    font-weight: 700;
-    text-shadow: 1px 2px 4px rgba(0,0,0,0.25);
-}
-</style>
