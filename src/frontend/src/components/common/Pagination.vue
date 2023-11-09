e1d675f65ba88d344cf3d0e1e779d01dcea45ba2
QuickOrBeDead <boraakgn@gmail.com>
2023-11-09 09:01:08 +0300

    add pager text change pager size

--- src/frontend/src/components/common/Pagination.vue
+++ src/frontend/src/components/common/Pagination.vue
@@ -4,7 +4,10 @@ import { ref, watch, onMounted } from 'vue'
 const emit = defineEmits(['pageChanged'])
 
 const props = defineProps<{
-  totalPages: number
+  totalPages: number | undefined,
+  totalCount: number | undefined,
+  pageSize: number | undefined,
+  page: number | undefined
 }>()
 
 let currentPage: number = 1
@@ -12,9 +15,14 @@ let nextPage = 0
 let previousPage = 0
 let start = 0
 let end = 0
-const pages =  ref<number[]>()
+const pages = ref<number[]>()
 
 watch(() => props.totalPages, () => {
+   updatePageList()
+})
+
+watch(() => props.page, () => {
+   currentPage = props.page ? props.page : 0
    updatePageList()
 })
 
@@ -31,14 +39,16 @@ function setCurrentPage(page: number)  {
 }
 
 function updatePageList() {
+    const totalPages = props.totalPages ? props.totalPages : 0
+
     start = Math.max(1, currentPage - 5)
-    end = Math.min(start + 10, props.totalPages)
+    end = Math.min(start + 10, totalPages)
     if (end - start < 10)
     {
         start = Math.max(1, end - 10)
     }
 
-    nextPage = Math.min(currentPage + 1, props.totalPages)
+    nextPage = Math.min(currentPage + 1, totalPages)
     previousPage = Math.max(currentPage - 1, start)
 
     const pageList: number[] = []
@@ -48,24 +58,41 @@ function updatePageList() {
 
     pages.value = pageList
 }
+
+function getPageSize() {
+    return props.pageSize ? props.pageSize : 0
+}
+
+function getTotalCount() {
+    return props.totalCount ? props.totalCount : 0
+}
 </script>
 
 <template>
- <ul class="pagination justify-content-center">
-    <li :class="[currentPage == start ? 'disabled': '']" class="page-item">
-        <a class="page-link" href="javascript:void(0)" @click="setCurrentPage(previousPage)" aria-label="Previous">
-            <span aria-hidden="true">«</span>
-        </a>
-    </li>
-    <template v-for="pageNumber in pages">
-        <li :class="[pageNumber == currentPage ? 'disabled': '']" class="page-item">
-            <a class="page-link" href="javascript:void(0)" @click="setCurrentPage(pageNumber)">{{ pageNumber }}</a>
-        </li>
-    </template>
-    <li :class="[currentPage == end ? 'disabled': '']" class="page-item">
-        <a class="page-link" href="javascript:void(0)" @click="setCurrentPage(nextPage)" aria-label="Next">
-            <span aria-hidden="true">»</span>
-        </a>
-    </li>
-</ul>
+    <div class="pagination-container">
+        <div class="float-start justify-content-start text-muted mb-2"><small class="align-bottom">Showing {{ ((currentPage - 1) * getPageSize()) + 1 }} to {{ Math.min(currentPage * getPageSize(), getTotalCount()) }} of {{ getTotalCount() }} rows</small></div>   
+        <ul class="float-end pagination pagination-sm justify-content-end mb-2">
+            <li :class="[currentPage == start ? 'disabled': '']" class="page-item">
+                <a class="page-link" href="javascript:void(0)" @click="setCurrentPage(previousPage)" aria-label="Previous">
+                    <span aria-hidden="true">«</span>
+                </a>
+            </li>
+            <template v-for="pageNumber in pages">
+                <li :class="[pageNumber == currentPage ? 'disabled': '']" class="page-item">
+                    <a class="page-link" href="javascript:void(0)" @click="setCurrentPage(pageNumber)">{{ pageNumber }}</a>
+                </li>
+            </template>
+            <li :class="[currentPage == end ? 'disabled': '']" class="page-item">
+                <a class="page-link" href="javascript:void(0)" @click="setCurrentPage(nextPage)" aria-label="Next">
+                    <span aria-hidden="true">»</span>
+                </a>
+            </li>
+        </ul>
+    </div>
 </template>
+<style scoped>
+    .pagination-container {
+        display: block;
+        clear: both;
+    }
+</style>
