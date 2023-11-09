58ee4233b304b14999d4d1d856e737dc9764d621
QuickOrBeDead <boraakgn@gmail.com>
2023-11-09 09:06:27 +0300

    add whitespace: 'preserve'

--- src/frontend/vite.config.ts
+++ src/frontend/vite.config.ts
@@ -3,7 +3,13 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue({
+    template: {
+      compilerOptions: {
+        whitespace: 'preserve'
+      }
+    }
+  })],
   server: {
     port: 8080
   }
