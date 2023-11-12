269e0e5987874cb52a1e887454b6448b8adeb481
QuickOrBeDead <boraakgn@gmail.com>
2023-11-12 03:39:36 +0300

    make settings edit component tab and tab contents col-12 and style

--- src/frontend/src/components/settings/Edit.vue
+++ src/frontend/src/components/settings/Edit.vue
@@ -14,9 +14,9 @@ const tabs: { [key: string]: any } = {
 <template>
     <div class="page-container">
         <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-md-8">
-                    <ul class="nav nav-tabs mb-3" role="tablist">
+            <div class="row">
+                <div class="col-md-12">
+                    <ul class="nav nav-tabs" role="tablist">
                         <li class="nav-item" role="presentation" v-for="(_, tab) in tabs">
                             <button @click="() => currentTab = tab as string" :class="['nav-link', { active: currentTab === tab }]" :id="`${tab}-tab-button`" data-bs-toggle="tab" type="button" role="tab" aria-controls="settings-tab" :aria-selected="currentTab === tab">
                                 {{ tab }}
@@ -24,9 +24,11 @@ const tabs: { [key: string]: any } = {
                         </li>
                     </ul>
                 </div>
-                <div class="tab-content">
-                    <div class="tab-pane fade show active" id="settings-tab" role="tabpanel" :aria-labelledby="`${currentTab}-tab-button`">
-                        <component :is="tabs[currentTab] as string"></component>  
+                <div class="col-md-12">
+                    <div class="tab-content">
+                        <div class="tab-pane fade show active" id="settings-tab" role="tabpanel" :aria-labelledby="`${currentTab}-tab-button`">
+                            <component :is="tabs[currentTab] as string"></component>  
+                        </div>
                     </div>
                 </div>
             </div>
