a5f0570661a5ad00045151002e784485167dafa7
QuickOrBeDead <boraakgn@gmail.com>
2023-11-15 22:11:20 +0300

    change jobs edit component ui design

--- src/frontend/src/components/jobs/Edit.vue
+++ src/frontend/src/components/jobs/Edit.vue
@@ -73,46 +73,52 @@ async function save() {
         <div class="container">
             <div class="row">
                 <div class="col-12">
-                    <h1 class="display-6">{{ isInEditMode ? "Edit" : "Add" }} Job</h1>
-                    <hr style="margin: 4px 0px;">
-                </div>
-            </div>
-            <div class="row justify-content-center">
-                <div class="col-6">
-                    <div class="row g-3">
-                        <div class="col-md-6">
-                            <label for="JobName" class="form-label">Job Name</label>
-                            <input id="JobName" type="text" class="form-control" v-model="job.jobName"/>
+                    <div class="card flex-fill">
+                        <div class="card-header d-flex justify-content-between align-items-center">
+                            <h4 class="card-title mb-0 text-muted"><small>{{ isInEditMode ? "Edit" : "Add" }} Job</small></h4>
+                            <router-link class="nav-link" to="/jobs" custom v-slot="{ navigate }">
+                                <button type="button" @click="navigate" class="btn btn-sm btn-outline-primary rounded-pill"><i class="bi bi-backspace"></i> Back</button>
+                            </router-link>
                         </div>
-                        <div class="col-md-6">
-                            <label for="JobGroup" class="form-label">Job Group</label>
-                            <input id="JobGroup" type="text" class="form-control" v-model="job.jobGroup"/>
+                        <div class="card-body">
+                            <div class="col-12">
+                                <div class="row g-3 mb-3">
+                                    <div class="col-md-6">
+                                        <label for="JobName" class="form-label">Job Name</label>
+                                        <input id="JobName" type="text" class="form-control" v-model="job.jobName"/>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <label for="JobGroup" class="form-label">Job Group</label>
+                                        <input id="JobGroup" type="text" class="form-control" v-model="job.jobGroup"/>
+                                    </div>
+                                    <div class="col-12">
+                                        <label for="Description" class="form-label">Description</label>
+                                        <input id="Description" type="text" class="form-control" v-model="job.description"/>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <label for="CronExpression" class="form-label">Cron Expression</label>
+                                        <input id="CronExpression" type="text" class="form-control" v-model="job.cronExpression" aria-describedby="helpCron"/>
+                                        <div id="helpCron" class="form-text text-black-50">{{ job.cronExpressionDescription }}</div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <label for="Package" class="form-label">Package</label>
+                                        <select id="Package" class="form-select" v-model="job.package">
+                                            <option v-for="option in packages" :key="option" :value="option">
+                                                {{ option }}
+                                            </option>
+                                        </select>
+                                    </div>
+                                    <div class="col-12">
+                                        <label for="Parameters" class="form-label">Parameters</label>
+                                        <textarea id="Parameters" class="form-control" v-model="(job.parameters as string)" rows="5" cols="30"></textarea>
+                                    </div>
+                                </div>
+                                <button class="btn btn-primary" @click="save">{{ isInEditMode ? "Edit" : "Add" }} Job</button>
+                            </div>
                         </div>
-                        <div class="col-12">
-                            <label for="Description" class="form-label">Description</label>
-                            <input id="Description" type="text" class="form-control" v-model="job.description"/>
-                        </div>
-                        <div class="col-md-6">
-                            <label for="CronExpression" class="form-label">Cron Expression</label>
-                            <input id="CronExpression" type="text" class="form-control" v-model="job.cronExpression" aria-describedby="helpCron"/>
-                            <div id="helpCron" class="form-text text-black-50">{{ job.cronExpressionDescription }}</div>
-                        </div>
-                        <div class="col-md-6">
-                            <label for="Package" class="form-label">Package</label>
-                            <select id="Package" class="form-select" v-model="job.package">
-                                <option v-for="option in packages" :key="option" :value="option">
-                                    {{ option }}
-                                </option>
-                            </select>
-                        </div>
-                        <div class="col-12">
-                            <label for="Parameters" class="form-label">Parameters</label>
-                            <textarea id="Parameters" class="form-control" v-model="(job.parameters as string)" rows="5" cols="30"></textarea>
-                        </div>
-                        <button class="btn btn-primary" @click="save">{{ isInEditMode ? "Edit" : "Add" }} Job</button>
                     </div>
                 </div>
-            </div>
+            </div>   
         </div>
     </div>
 </template>
