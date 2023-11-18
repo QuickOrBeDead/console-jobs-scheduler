dc9251a0345f311edb411056cef501d919a7eedc
QuickOrBeDead <boraakgn@gmail.com>
2023-11-18 23:15:59 +0300

    fix roslyn:SYSLIB0051

--- src/backend/src/ConsoleJobScheduler.Service/Infrastructure/Scheduler/Jobs/Exceptions/ConsoleAppPackageRunFailException.cs
+++ src/backend/src/ConsoleJobScheduler.Service/Infrastructure/Scheduler/Jobs/Exceptions/ConsoleAppPackageRunFailException.cs
@@ -1,8 +1,6 @@
 ﻿namespace ConsoleJobScheduler.Service.Infrastructure.Scheduler.Jobs.Exceptions;
 
-using System.Runtime.Serialization;
-
-public class ConsoleAppPackageRunFailException : Exception
+public sealed class ConsoleAppPackageRunFailException : Exception
 {
     public int ExitCode { get; }
 
@@ -21,17 +19,5 @@ public class ConsoleAppPackageRunFailException : Exception
         : base(message)
     {
         ExitCode = exitCode;
-    }
-
-    protected ConsoleAppPackageRunFailException(SerializationInfo info, StreamingContext context)
-        : base(info, context)
-    {
-        ExitCode = info.GetInt32("ExitCode");
-    }
-
-    public override void GetObjectData(SerializationInfo info, StreamingContext context)
-    {
-        base.GetObjectData(info, context);
-        info.AddValue("ExitCode", ExitCode, typeof(int));
     }
 }
